91af5257f105d744d3f60d48d42e9cb17e4f89e4
Pascal-Ahmadu <pascalally41@gmail.com>
2026-08-13 18:32:58 +0100

    fix notifications segment rooms compile errors

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -24,7 +24,7 @@ export class AuthController{
     constructor(
         private readonly authService: AuthService,
         private readonly otpService: OtpService,
-        private readonly redis: Redis,
+       
     ){}
 
 
