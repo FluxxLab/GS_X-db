167538eb35a81c4fc8af651a09f56a2bc812bc4a
Pascal-Ahmadu <pascalally41@gmail.com>
2026-08-13 18:28:08 +0100

    fix notifications segment rooms compile errors

--- src/discussions/discussions.module.ts
+++ src/discussions/discussions.module.ts
@@ -1,10 +1,18 @@
-import { DelegateModule } from '../delegate/delegate.module';
+import { Module } from "@nestjs/common";
+import { DiscussionsGateway } from "./discussions.gateway";
+import { DiscussionService } from "./discussions.service";
+import { DiscussionsController } from "./discussions.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { SessionComment } from "./entities/session-comment.entity";
+import { SessionsService } from "../sessions/sessions.service";
+import { RealtimeService } from "../common/realtime/realtime.service";
+import { SessionsModule } from "src/sessions/sessions.module";
+import { Delegate } from "src/delegate/entities/delegate.entity";
+import { DelegateModule } from "src/delegate/delegate.module";
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([SessionComment]),
-    SessionsModule,
-    DelegateModule,          // ← the missing line
-  ],
-  ...
+    imports: [TypeOrmModule.forFeature([SessionComment]), SessionsModule, DelegateModule],
+    providers: [DiscussionsGateway, DiscussionService, RealtimeService],
+    controllers: [DiscussionsController,]
 })
+export class DiscussionsModule {}
