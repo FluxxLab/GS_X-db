e5dfb5268ae8119f55c4fea9af5767fe5c1906a3
Pascal-Ahmadu <pascalally41@gmail.com>
2026-08-21 13:38:58 +0100

    deepgram fix

--- src/captions/captions.module.ts
+++ src/captions/captions.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SessionsModule } from '../sessions/sessions.module';
+import { CaptionsController } from './captions.controller';
 import { CaptionsGateway } from './captions.gateway';
 import { CaptionsService } from './captions.service';
+import { LivekitService } from './livekit.service';
 import { TranscriptSegment } from './entities/transcript-segment.entity';
 import { DeepTranscriptionProvider } from './transcription/deepgram.provider';
 import { FakeTranscriptionProvider } from './transcription/fake.provider';
@@ -11,9 +13,11 @@ import { TRANSCRIPTION_PROVIDER } from './transcription/transcription.interface'
 
 @Module({
   imports: [TypeOrmModule.forFeature([TranscriptSegment]), SessionsModule],
+  controllers: [CaptionsController],
   providers: [
     CaptionsService,
     CaptionsGateway,
+    LivekitService,
     {
       provide: TRANSCRIPTION_PROVIDER,
       inject: [ConfigService],
@@ -23,6 +27,6 @@ import { TRANSCRIPTION_PROVIDER } from './transcription/transcription.interface'
           : new FakeTranscriptionProvider(),
     },
   ],
-  exports: [CaptionsService],
+  exports: [CaptionsService, LivekitService],
 })
-export class CaptionsModule {}
+export class CaptionsModule { }
